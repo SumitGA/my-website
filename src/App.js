73730128bea13bef7114afbb41c502b3c7957a2d
SumitGA <sghost33@gmail.com>
2018-10-13 13:46:59 +0545

    Setting up Redux React-Redux Redux-Thunk Redux-Logger Redux-CrashReport

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
+    <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -21,6 +24,7 @@ class App extends Component {
           </a>
         </header>
       </div>
+    </Provider>
     );
   }
 }
