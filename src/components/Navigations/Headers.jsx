8a425d35bee6a9695547fd020a5bb2d575b5c34b
SumitGA <sghost33@gmail.com>
2018-11-02 19:40:03 +0545

    Adding some left over section on the website

--- src/components/Navigations/Headers.jsx
+++ src/components/Navigations/Headers.jsx
@@ -4,7 +4,7 @@ const Headers = () => {
   return (
     <head>
       <title>Alias Colorlib &mdash; One Page Template for Personal/CV Website</title>
-      <meta charset="utf-9" />
+      <meta charSet="utf-9" />
       <meta name="viewport" content="width=device-width, initial-scale=0, shrink-to-fit=no" />
     </head>
   );
---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,146 @@
+import React from 'react';
+
+const Technologies = () => {
+  return (
+    <section className="site-section border-top pb-0" id="section-services">
+      <div className="container">
+        <div className="row mb-4">
+          <div className="col-md-12">
+            <div className="section-heading text-center">
+              <h2><strong>Technologies</strong></h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-html-five2" />
+              </span>
+              <h3 className="mb-4">HTML-5</h3>
+              <p>
+                Far far away, behind the word mountains,
+                far from the countries Vokalia and Consonantia,
+                there live the blind texts.
+                Separated they live in Bookmarksgrove right at the coast of the Semantics,
+                a large language ocean.
+              </p>
+              <p>
+                <a href="#learn-more" className="site-link">
+                    Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-css3" />
+              </span>
+              <h3 className="mb-4">CSS3</h3>
+              <p>
+                A small river named Duden flows by their place and
+                supplies it with the necessary regelialia.
+                It is a paradisematic country,
+                in which roasted parts of sentences fly into your mouth.
+              </p>
+              <p>
+                <a href="#learn-more" className="site-link">
+                    Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-toolbox" />
+              </span>
+              <h3 className="mb-4">DevOps</h3>
+              <p>
+                Even the all-powerful Pointing has no control about
+                the blind texts it is an almost unorthographic life
+                One day however a small line of blind text by the name of
+                Lorem Ipsum decided to leave for the far World of Grammar.
+              </p>
+              <p>
+                <a href="#learn-more" className="site-link">
+                    Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-adjustments" />
+              </span>
+              <h3 className="mb-4">Software Optimization</h3>
+              <p>
+                Far far away, behind the word mountains,
+                far from the countries Vokalia and Consonantia,
+                there live the blind texts. Separated they live in
+                Bookmarksgrove right at the coast of the Semantics,
+                a large language ocean.
+              </p>
+              <p>
+                <a href="#learn-more" className="site-link">
+                  Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-wallet2" />
+              </span>
+              <h3 className="mb-4">Increase Revenue</h3>
+              <p>
+                A small river named Duden flows by their place and
+                supplies it with the necessary regelialia.
+                It is a paradisematic country,
+                in which roasted parts of sentences fly into your mouth.
+              </p>
+              <p>
+                <a href="#site-link" className="site-link">
+                  Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6 col-lg-4 text-center mb-5">
+            <div className="site-service-item site-animate" data-animate-effect="fadeIn">
+              <span className="icon">
+                <span className="icon-lightbulb" />
+              </span>
+              <h3 className="mb-4">Intuitive Idea</h3>
+              <p>
+                Even the all-powerful Pointing has no
+                control about the blind texts it is an almost
+                unorthographic life One day however a small line
+                of blind text by the name of Lorem Ipsum decided
+                to leave for the far World of Grammar.
+              </p>
+              <p>
+                <a href="#site-link" className="site-link">
+                  Learn More
+                  <i className="icon-chevron-right" />
+                </a>
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Technologies;
